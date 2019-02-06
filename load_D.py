8a434256861cbf08abb121628e9bb5fece0ca8a7
McCourt Hu <evergreen8998@gmail.com>
2019-02-06 11:22:20 -0500

    load discriminator in load_D.py

---
+++ load_D.py
@@ -0,0 +1,10 @@
+import torch
+from models.Discriminator_VGG import Discriminator_VGG_128
+from imageio import imread
+import numpy as np
+
+ckpt = torch.load("./50000_D.pth")
+model = Discriminator_VGG_128()
+model.load_state_dict(ckpt)
+hr = imread('imgs/hr.png')
+model(torch.Tensor(np.moveaxis(np.expand_dims(hr, 0), -1, 1)))
