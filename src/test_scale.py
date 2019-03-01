3b63e75e304b39a728fb66fe64e28d39844e4498
McCourt Hu <evergreen8998@gmail.com>
2019-03-01 11:34:43 -0500

    Update test_scale.py

--- src/test_scale.py
+++ src/test_scale.py
@@ -14,8 +14,14 @@ for img_name in sorted(os.listdir('imgs/TrainGT/')):
     for scale in np.linspace(2, 4, num=5):
         for order_1 in range(6):
             for order_2 in range(6):
-                mse = np.mean(np.square(resize(resize(hr, (h // scale, w // scale), mode='reflect', preserve_range=True, order=order_1), 
-                                               (h, w), mode='reflect', preserve_range=True, order=order_2) - hr))
+                mse = np.mean(np.square(
+                    resize(
+                        resize(
+                            hr, (h // scale, w // scale), mode='reflect', preserve_range=True, order=order_1
+                        ),
+                        (h, w), mode='reflect', preserve_range=True, order=order_2
+                    ) - hr
+                ))
                 if max_mse is None or mse < max_mse:
                     max_mse = mse
                     print('{} {} {} {} {}'.format(img_name, scale, order_1, order_2, max_mse), end='\r')
