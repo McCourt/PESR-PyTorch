e9e25ba3cb3f9a856de3ee1a8d93e6d3e15f40e1
McCourt Hu <evergreen8998@gmail.com>
2019-03-01 11:37:25 -0500

    Update and rename load_D.py to src/test_D.py

--- src/test_D.py
+++ src/test_D.py
@@ -3,8 +3,8 @@ from model.discriminator.Discriminator_VGG import Discriminator_VGG_128
 from imageio import imread
 import numpy as np
 
-ckpt = torch.load("./50000_D.pth")
+ckpt = torch.load("vgg_discriminator.ckpt")
 model = Discriminator_VGG_128()
 model.load_state_dict(ckpt)
-hr = imread('imgs/hr.png')
+hr = imread('../imgs/hr.png')
 model(torch.Tensor(np.moveaxis(np.expand_dims(hr, 0), -1, 1)))
