a2d73a27b438d5058f8008dba35ec1a9fe26aac6
McCourt Hu <evergreen8998@gmail.com>
2019-03-06 10:05:53 -0500

    add test mode to train file

--- trainer.py
+++ trainer.py
@@ -1,6 +1,6 @@
 from torch.utils.data import DataLoader
 from src.helper import *
-from src.dataset import SRTrainDataset
+from src.dataset import SRTrainDataset, SRTestDataset
 import os, sys
 import getopt
 from time import time
@@ -10,7 +10,7 @@ if __name__ == '__main__':
 
     # Load system arguments
     args = sys.argv[1:]
-    long_opts = ['model-name=', 'scale=']
+    long_opts = ['mode=']
 
     try:
         optlist, args = getopt.getopt(args, '', long_opts)
@@ -19,12 +19,10 @@ if __name__ == '__main__':
         sys.exit(2)
 
     for arg, opt in optlist:
-        if arg == '--model-name':
-            model_name = str(opt)
-            sr_model = load_model(model_name)
-            # ds_model = load_model(model_name)
-        elif arg == '--scale':
-            scale = int(opt)
+        if arg == '--mode':
+            mode = str(opt)
+            if mode not in ['train', 'test']:
+                raise Exception('Wrong pipeline mode detected')
         else:
             raise Exception("Redundant Argument Detected")
 
@@ -33,68 +31,104 @@ if __name__ == '__main__':
         params = load_parameters()
         print('Parameters loaded')
         print(''.join(['-' for i in range(30)]))
-        train_params = params['train']
+        pipeline_params = params[mode]
         common_params = params['common']
-        for i in train_params:
-            print('{:<15s} -> {}'.format(str(i), train_params[i]))
+        for i in pipeline_params:
+            print('{:<15s} -> {}'.format(str(i), pipeline_params[i]))
     except Exception as e:
         print(e)
         raise ValueError('Parameter not found.')
 
-    # Prepare all directory and devices
-    device = torch.device('cuda:{}'.format(train_params['device_ids']) if torch.cuda.is_available else 'cpu')
-    lr_dir = os.path.join(common_params['root_dir'], common_params['lr_dir'])
-    hr_dir = os.path.join(common_params['root_dir'], common_params['hr_dir'])
-    sr_dir = os.path.join(common_params['root_dir'], common_params['sr_dir'].format(model_name))
-    log_dir = os.path.join(common_params['root_dir'], common_params['train_log'].format(model_name))
-    ckpt_dir = os.path.join(common_params['root_dir'], common_params['ckpt_dir'].format(model_name))
+    # Prepare common parameters
+    device = torch.device('cuda:{}'.format(pipeline_params['device_ids']) if torch.cuda.is_available else 'cpu')
+    root_dir = common_params['root_dir']
+    if common_params['up_sampler'] is not None:
+        up_sampler = common_params['up_sampler']
+    else:
+        raise Exception('You must define a upscale model for super resolution')
+    down_sampler = common_params['down_sampler']
+    scale = common_params['scale']
 
-    # Define model and loss function
+    # Prepare all directory and devices
+    lr_dir = os.path.join(root_dir, common_params['lr_dir'])
+    hr_dir = os.path.join(root_dir, common_params['hr_dir'])
+    sr_dir = os.path.join(root_dir, common_params['sr_dir'].format(up_sampler))
+    log_dir = os.path.join(root_dir, common_params['log_dir'].format(mode, up_sampler))
+    sr_ckpt = os.path.join(root_dir, common_params['ckpt_dir'].format(up_sampler))
+    if down_sampler is not None:
+        ds_ckpt = os.path.join(root_dir, common_params['ckpt_dir'].format(down_sampler))
+
+    # Define upscale model and data parallel
+    sr_model = load_model(up_sampler)
     sr_model = nn.DataParallel(sr_model).cuda()
+
+    # Define downscale model and data parallel
+    if down_sampler is not None:
+        ds_model = load_model(down_sampler)
+        ds_model = nn.DataParallel(ds_model).cuda()
+
+    # Define loss functions
     sr_loss = nn.MSELoss(reduction='elementwise_mean').to(device)
-    lr_loss = nn.MSELoss(reduction='elementwise_mean').to(device)
-    lr_loss.require_grad = False
-    sr_model = sr_model.cuda()
+    if down_sampler is not None:
+        ds_loss = nn.MSELoss(reduction='elementwise_mean').to(device)
+    real_loss = nn.MSELoss(reduction='elementwise_mean').to(device)
+    real_loss.require_grad = False
 
     # Define optimizer and learning rate scheduler
+    params = sr_model.parameters()
+    if down_sampler is not None:
+        params = list(params) + list(ds_model.parameters())
     optimizer = torch.optim.Adam(
-        sr_model.parameters(),
-        lr=train_params['learning_rate']
+        params,
+        lr=pipeline_params['learning_rate']
     )
     scheduler = torch.optim.lr_scheduler.ExponentialLR(
         optimizer,
-        gamma=train_params['decay_rate']
+        gamma=pipeline_params['decay_rate']
     )
 
-    # Define data source
-    dataset = SRTrainDataset(
-        hr_dir=hr_dir,
-        lr_dir=lr_dir,
-        h=train_params['window'][0],
-        w=train_params['window'][1],
-        scale=scale,
-        num_per=train_params['num_per']
-    )
+    # Define data source and data loader
+    if mode == 'train':
+        dataset = SRTrainDataset(
+            hr_dir=hr_dir,
+            lr_dir=lr_dir,
+            h=pipeline_params['window'][0],
+            w=pipeline_params['window'][1],
+            scale=scale,
+            num_per=pipeline_params['num_per']
+        )
+    else:
+        dataset = SRTestDataset(
+            hr_dir=hr_dir,
+            lr_dir=lr_dir
+        )
     data_loader = DataLoader(
         dataset,
-        batch_size=train_params['batch_size'],
-        shuffle=True,
-        num_workers=train_params['num_worker']
+        batch_size=pipeline_params['batch_size'] if mode == 'train' else 1,
+        shuffle=True if mode == 'train' else False,
+        num_workers=pipeline_params['num_worker']
     )
 
     # Load checkpoint
     begin_epoch = 0
-    ckpt = load_checkpoint(
-        load_dir=ckpt_dir,
-        map_location=train_params['map_location']
+    sr_checkpoint = load_checkpoint(
+        load_dir=sr_ckpt,
+        map_location=pipeline_params['map_location']
     )
+    if down_sampler is not None:
+        ds_checkpoint = load_checkpoint(
+            load_dir=ds_ckpt,
+            map_location=pipeline_params['map_location']
+        )
     try:
-        if ckpt is None:
+        if sr_checkpoint is None:
             print('Start new training')
         else:
             print('recovering from checkpoints', end='\r')
-            sr_model.load_state_dict(ckpt['model'])
-            begin_epoch = ckpt['epoch'] + 1
+            sr_model.load_state_dict(sr_checkpoint['model'])
+            if down_sampler is not None:
+                ds_model.load_state_dict(ds_checkpoint['model'])
+            begin_epoch = sr_checkpoint['epoch'] + 1
             print('resuming training from epoch {}'.format(begin_epoch))
     except Exception as e:
         raise ValueError('Checkpoint not found.')
@@ -103,7 +137,7 @@ if __name__ == '__main__':
     print('Using device {}'.format(device))
     title_formatter = '{:^6s} | {:^6s} | {:^8s} | {:^8s} | {:^8s} | {:^8s} | {:^8s} | {:^10s} '
     report_formatter = '{:^6d} | {:^6d} | {:^8.2f} | {:^8.2f} | {:^8.2f} | {:^8.2f} | {:^8.2f} | {:^10.2f}'
-    title = title_formatter.format('Epoch', 'Batch', 'BLoss', 'ELoss', 'BPSNR', 'EPSNR', 'RPSNR', 'Runtime')
+    title = title_formatter.format('Epoch', 'Batch', 'BLoss', 'ELoss', 'SRPSNR', 'DSPSNR', 'RPSNR', 'Runtime')
     splitter = ''.join(['-' for i in range(len(title))])
     print(splitter)
     begin = time()
@@ -111,31 +145,47 @@ if __name__ == '__main__':
     print(title)
     print(splitter)
     with open(log_dir, 'w') as f:
-        for epoch in range(begin_epoch, train_params['num_epoch']):
-            e_ls = []
+        for epoch in range(begin_epoch, pipeline_params['num_epoch']):
+            epoch_ls = []
+            ds_l, ds_psnr = None, None
             for bid, batch in enumerate(data_loader):
                 hr, lr = batch['hr'].to(device), batch['lr'].to(device)
                 optimizer.zero_grad()
                 sr = sr_model(lr)
-                b_loss = sr_loss(sr, hr)
-                r_loss = lr_loss(lr, hr)
-                b_loss.backward()
+                sr_l = sr_loss(sr, hr)
+                sr_psnr = psnr(sr_l)
+                if down_sampler is not None:
+                    dsr = ds_model(sr)
+                    ds_l = ds_loss(dsr, lr)
+                    l = ds_l + sr_l
+                    ds_psnr = psnr(ds_l)
+                else:
+                    l = sr_l
+                real_l = real_loss(lr, hr)
+                real_psnr = psnr(real_l)
+
+                l.backward()
                 optimizer.step()
-                e_ls.append(b_loss)
-                e_loss = sum(e_ls) / (bid + 1)
-                report = report_formatter.format(epoch, bid, b_loss, e_loss, psnr(b_loss),
-                                                 psnr(e_loss), psnr(r_loss), since(begin))
-                if bid % train_params['print_every'] == 0:
+
+                epoch_ls.append(l)
+                ep_l = sum(epoch_ls) / (bid + 1)
+                report = report_formatter.format(epoch, bid, l, ep_l, sr_psnr, ds_psnr, real_psnr, since(begin))
+                if bid % pipeline_params['print_every'] == 0:
                     print(report)
                     print(title, end='\r')
 
                 f.write(report + '\n')
                 f.flush()
             scheduler.step()
-            if epoch % train_params['save_every'] == 0 or epoch == train_params['num_epoch'] - 1:
+            if epoch % pipeline_params['save_every'] == 0 or epoch == pipeline_params['num_epoch'] - 1:
                 state_dict = {
                     'model': sr_model.state_dict(),
                     'epoch': epoch
                 }
-                save_checkpoint(state_dict, ckpt_dir)
+                save_checkpoint(state_dict, sr_ckpt)
+                if down_sampler is not None:
+                    state_dict = {
+                        'model': ds_model.state_dict(),
+                    }
+                    save_checkpoint(state_dict, ds_ckpt)
             print(splitter)
