3b5ec8b851e81168b187b80b10ab845385ae58b5
McCourt Hu <evergreen8998@gmail.com>
2019-03-08 12:43:28 -0500

    change to L1 loss

--- trainer.py
+++ trainer.py
@@ -69,11 +69,11 @@ if __name__ == '__main__':
         ds_model = nn.DataParallel(ds_model, device_ids=common_params['device_ids']).cuda()
 
     # Define loss functions
-    sr_loss = nn.MSELoss(reduction='elementwise_mean').to(device)
+    sr_loss = nn.L1Loss().to(device)
     if down_sampler is not None:
-        ds_loss = nn.MSELoss(reduction='elementwise_mean').to(device)
-    real_loss = nn.MSELoss(reduction='elementwise_mean').to(device)
-    real_loss.require_grad = False
+        ds_loss = nn.L1Loss().to(device)
+    mse_loss = nn.MSELoss(reduction='elementwise_mean').to(device)
+    mse_loss.require_grad = False
 
     # Define optimizer and learning rate scheduler
     params = sr_model.parameters()
@@ -161,19 +161,19 @@ if __name__ == '__main__':
 
                 sr = sr_model(lr)
                 sr_l = sr_loss(sr, hr)
-                sr_psnr = psnr(sr_l)
+                sr_psnr = psnr(torch.nn.functional.mse_loss(sr, hr)).item()
                 epoch_sr.append(sr_psnr)
 
                 if down_sampler is not None:
                     dsr = ds_model(sr)
                     ds_l = ds_loss(dsr, lr)
                     l = pipeline_params['lambda'] * ds_l + sr_l
-                    ds_psnr = psnr(ds_l)
+                    ds_psnr = psnr(torch.nn.functional.mse_loss(dsr, lr)).item()
                 else:
                     l = sr_l
                 epoch_lr.append(ds_psnr)
 
-                real_l = real_loss(lr, hr)
+                real_l = mse_loss(lr, hr)
                 real_psnr = psnr(real_l)
 
                 diff = sr_psnr - real_psnr
