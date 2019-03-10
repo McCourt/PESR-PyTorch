95023f5f0ad638ac0023ccbf9d1274518802637d
Alex Damian <ad315@duke.edu>
2019-03-10 05:27:52 -0400

    Update shiftLoss.py
    Fixed a mistake but not sure which bicubic downscaler to use.

--- shiftLoss.py
+++ shiftLoss.py
@@ -1,17 +1,20 @@
 import torch
 from torch.nn import functional as F
+from model.downscaler.bicubic import BicubicDownSample
+import imresize as im
 
 class ShiftLoss(torch.nn.Module):
     def __init__(self):
-        self.kernel_dict = torch.load('shifts10.pt')
         super(ShiftLoss, self).__init__()
+        self.kernel_dict = torch.load('shifts10.pt')
+        self.bicubic = BicubicDownSample()
 
     def forward(self, lr, hr):
         loss = 0
         for i in range(-2, 2):
             for j in range(-2, 2):
                 x = F.conv2d(input=lr, weight=self.kernel_dict[(i, j)], stride=1)
-                y = hr[:, :, 10 + i:-10 + i, 10 + j:-10 + j]
+                y = self.bicubic(hr.roll(0,0,i,j))[:, :, 10:-10, 10:-10]
                 loss += F.mse_loss(x, y)
         loss = loss / 16
         return loss
