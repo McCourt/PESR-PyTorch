cc19259a58ac51667cd8bd811e11858293008c2e
McCourt Hu <evergreen8998@gmail.com>
2019-03-13 14:24:57 -0400

    shift loss update

--- loss/shiftloss.py
+++ loss/shiftloss.py
@@ -14,7 +14,7 @@ class ShiftLoss(torch.nn.Module):
         for i in range(-2, 2):
             for j in range(-2, 2):
                 x = F.conv2d(input=lr, weight=self.kernel_dict[(i, j)].unsqueeze_(0).type('torch.cuda.FloatTensor'), stride=1)
-                y = self.bicubic(hr.roll((0 ,0 ,i ,j)))[:, :, 10:-10, 10:-10]
+                y = self.bicubic(hr.roll((0, 0, i, j)))[:, :, 10:-10, 10:-10]
                 loss += F.mse_loss(x, y)
         loss = loss / 16
         return loss
