3b3592c07e6e5093004c5da6c43b03a9aa3ec106
McCourt Hu <sh395@cs.duke.edu>
2019-03-13 01:39:36 -0400

    unrunnable f*cking code

--- loss/shiftloss.py
+++ loss/shiftloss.py
@@ -13,8 +13,8 @@ class ShiftLoss(torch.nn.Module):
         loss = 0
         for i in range(-2, 2):
             for j in range(-2, 2):
-                x = F.conv2d(input=lr, weight=self.kernel_dict[(i, j)], stride=1)
-                y = self.bicubic(hr.roll(0 ,0 ,i ,j))[:, :, 10:-10, 10:-10]
+                x = F.conv2d(input=lr, weight=self.kernel_dict[(i, j)].unsqueeze_(0).type('torch.cuda.FloatTensor'), stride=1)
+                y = self.bicubic(hr.roll((0 ,0 ,i ,j)))[:, :, 10:-10, 10:-10]
                 loss += F.mse_loss(x, y)
         loss = loss / 16
         return loss
