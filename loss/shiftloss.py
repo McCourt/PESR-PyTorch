9ad0bf2c92957370521f39c7130d03aa12e23e9d
McCourt Hu <sh395@cs.duke.edu>
2019-03-13 19:17:14 -0400

    changes

--- loss/shiftloss.py
+++ loss/shiftloss.py
@@ -9,16 +9,14 @@ class ShiftLoss(nn.Module):
         super(ShiftLoss, self).__init__()
         self.kernel_dict = torch.load(kernel_dir)
         self.bicubic = BicubicDownSample()
-        for key in self.kernel_dict.keys():
-            self.kernel_dict[key] = self.kernel_dict[key].unsqueeze_(0)
 
     def forward(self, hr, lr):
         loss = 0
         for i in range(-2, 2):
             for j in range(-2, 2):
                 kernel = self.kernel_dict[(i, j)].type('torch.cuda.FloatTensor')
-                x = F.conv2d(input=lr, weight=kernel, stride=1, padding=3)
-                y = self.bicubic(hr.roll((i, j), (2, 3)))
+                x = F.conv2d(input=lr, weight=kernel, stride=1)
+                y = self.bicubic(hr.roll((i, j), (2, 3)))[:, :, 10:-10, 10:-10]
                 loss += F.mse_loss(x, y)
         loss = loss / 16
         return loss
