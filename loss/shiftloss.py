56a78941b7828fc4f50e4a75111bff1e1652b528
McCourt Hu <evergreen8998@gmail.com>
2019-03-13 18:20:45 -0400

    update training and tto files

--- loss/shiftloss.py
+++ loss/shiftloss.py
@@ -15,10 +15,9 @@ class ShiftLoss(nn.Module):
         for i in range(-2, 2):
             for j in range(-2, 2):
                 kernel = self.kernel_dict[(i, j)].unsqueeze_(0).type('torch.cuda.FloatTensor')
-                print(kernel.size())
-                x = F.conv2d(input=lr, weight=kernel, stride=1, padding=3)
+                print(kernel.size(), lr.size())
+                x = F.conv2d(input=lr, weight=kernel, stride=1, padding=3, group=3)
                 y = self.bicubic(hr.roll((i, j), (2, 3)))#[:, :, 10:-10, 10:-10]
-                print(x.size(), y.size())
                 loss += F.mse_loss(x, y)
         loss = loss / 16
         return loss
