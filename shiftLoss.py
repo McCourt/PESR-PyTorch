ad9bc6a11fe7fa45c57e247713efe477f76e8d2b
Alex Damian <ad315@duke.edu>
2019-03-13 18:57:46 -0400

    Update shiftLoss.py
    Fixed the tensor.roll command

--- shiftLoss.py
+++ shiftLoss.py
@@ -14,7 +14,7 @@ class ShiftLoss(torch.nn.Module):
         for i in range(-2, 2):
             for j in range(-2, 2):
                 x = F.conv2d(input=lr, weight=self.kernel_dict[(i, j)], stride=1)
-                y = self.bicubic(hr.roll(0,0,i,j))[:, :, 10:-10, 10:-10]
+                y = self.bicubic(hr.roll((i,j),(2,3))[:, :, 10:-10, 10:-10]
                 loss += F.mse_loss(x, y)
         loss = loss / 16
         return loss
