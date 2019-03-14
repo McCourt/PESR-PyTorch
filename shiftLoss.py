a945e46c4e062dfa535f0b9e3bca8750ac8533f5
Alex Damian <ad315@duke.edu>
2019-03-13 20:14:17 -0400

    Update shiftLoss.py
    Made the image roll in the opposite direction

--- shiftLoss.py
+++ shiftLoss.py
@@ -1,20 +1,22 @@
 import torch
-from torch.nn import functional as F
+import torch.nn as nn
+import torch.nn.functional as F
 from model.downscaler.bicubic import BicubicDownSample
-import imresize as im
 
-class ShiftLoss(torch.nn.Module):
-    def __init__(self):
+
+class ShiftLoss(nn.Module):
+    def __init__(self, kernel_dir='checkpoints/shifts.ckpt'):
         super(ShiftLoss, self).__init__()
-        self.kernel_dict = torch.load('shifts.pt')
+        self.kernel_dict = torch.load(kernel_dir)
         self.bicubic = BicubicDownSample()
 
-    def forward(self, lr, hr):
+    def forward(self, hr, lr):
         loss = 0
         for i in range(-2, 2):
             for j in range(-2, 2):
-                x = F.conv2d(input=lr, weight=self.kernel_dict[(i, j)], stride=1)
-                y = self.bicubic(hr.roll((i,j),(2,3))[:, :, 10:-10, 10:-10]
+                kernel = self.kernel_dict[(i, j)].type('torch.cuda.FloatTensor')
+                x = F.conv2d(input=lr, weight=kernel, stride=1)
+                y = self.bicubic(hr.roll((-i,-j), (2, 3)))[:, :, 10:-10, 10:-10]
                 loss += F.mse_loss(x, y)
         loss = loss / 16
         return loss
