a6643c5688c1aab739c36f7c7e1b68a81af25e3a
McCourt Hu <sh395@cs.duke.edu>
2019-04-03 14:25:17 -0400

    trim added

--- loss/dsloss.py
+++ loss/dsloss.py
@@ -3,11 +3,12 @@ from model.downscaler.bicubic import BicubicDownSample
 
 
 class DownScaleLoss(nn.Module):
-    def __init__(self):
+    def __init__(self, clip_round=False):
         super().__init__()
         self.down_sampler = BicubicDownSample()
+        self.clip_round = clip_round
         self.mse = nn.L1Loss()
         self.requires_grad = False
 
     def forward(self, sr, lr):
-        return self.mse(self.down_sampler(sr), lr)
+        return self.mse(self.down_sampler(sr, clip_round=self.clip_round), lr)
