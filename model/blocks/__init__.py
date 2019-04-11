f4bfd11017c48f53eddaf6f9da8b11931f94c7f1
McCourt Hu <evergreen8998@gmail.com>
2019-04-11 18:02:52 -0400

    reorganize blocks

--- model/blocks/__init__.py
+++ model/blocks/__init__.py
@@ -1,1 +1,4 @@
-from model.blocks.Blocks import *
+from model.blocks.Useful import *
+from model.blocks.InterBlocks import *
+from model.blocks.AttentionBlock import *
+from model.blocks.UpscaleBlock import *
