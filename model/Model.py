8d11d1eb1c68bea866dca6430637a03f74a57d07
McCourt Hu <evergreen8998@gmail.com>
2019-04-11 16:13:30 -0400

    update new model and model loading method

--- model/Model.py
+++ model/Model.py
@@ -1,10 +1,21 @@
+import torch
 import torch.nn as nn
 from importlib import import_module
 import json
 
 
+def load_checkpoint(load_dir, map_location=None):
+    try:
+        print('loading checkpoint from {}'.format(load_dir))
+        checkpoint = torch.load(load_dir, map_location=map_location)
+        print('loading successful')
+        return checkpoint
+    except:
+        print('No checkpoint and begin new training')
+
+
 class Model(nn.Module):
-    def __init__(self, name, mode, **kwargs):
+    def __init__(self, name, mode, checkpoint, train=True, map_location=None, **kwargs):
         super().__init__()
         with open('model/models.json', 'r') as f:
             params = json.load(f)
@@ -17,6 +28,20 @@ class Model(nn.Module):
         path = '.'.join(['model', mode, params[mode][name.lower()]])
         module = getattr(import_module(path), 'Model')
         self.model = module(**kwargs)
+        print('Number of parameters of SR model: {:.2E}'.format(sum(p.numel() for p in self.model.parameters() if p.requires_grad)))
+
+        try:
+            sr_checkpoint = load_checkpoint(load_dir=checkpoint, map_location=map_location)
+            if sr_checkpoint is None:
+                print('Start new training for {} model'.format(mode))
+            else:
+                print('{} model recovering from checkpoints'.format(mode))
+                self.model.load_state_dict(sr_checkpoint['model'])
+        except:
+            raise ValueError('Checkpoint not found.')
+
+        for param in self.model.parameters():
+            param.requires_grad = False if not train else param.requires_grad
 
     def forward(self, x):
         return self.model(x)
