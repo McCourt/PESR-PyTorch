56e03e7f1cf4192ef743a98cd18852dbdb8fcb95
McCourt Hu <evergreen8998@gmail.com>
2019-06-17 18:12:28 -0400

    ignore dataset

--- model/blocks/AttentionBlock.py
+++ model/blocks/AttentionBlock.py
@@ -1,6 +1,7 @@
 import torch
 from torch import nn
 from model.blocks import ConvolutionBlock
+from torch.nn import functional as F
 
 
 class Flatten(nn.Module):
@@ -45,6 +46,7 @@ class SpatialAttentionBlock(nn.Module):
     def forward(self, x):
         return x * self.model(x)
 
+
 class ShuffleAttention(nn.Module):
     def __init__(self, in_channels):
         super().__init__()
@@ -75,3 +77,24 @@ class ShuffleAttention(nn.Module):
         x = self.postproc(x)
         return in_tensor * x
 
+
+class ChannelAttV2(nn.Module):
+    def __init__(self, in_c, ratio=16.0, degrade=8, bias=True, act=nn.ReLU):
+        super().__init__()
+        re_c = int(round(in_c / ratio))
+        self.degrade = degrade
+        self.bottle_neck = nn.Sequential(
+            nn.Conv2d(in_channels=in_c, out_channels=re_c, kernel_size=1, bias=bias),
+            act(inplace=True),
+            nn.Conv2d(in_channels=re_c, out_channels=in_c, kernel_size=1, bias=bias)
+        )
+        self.output = nn.Softmax(dim=1)
+
+    def forward(self, x):
+        n, c, h, w = x.size()
+        kernel_size = min(h, w, self.degrade)
+        ds = F.avg_pool2d(x, kernel_size=kernel_size, padding=0)
+        ds = self.bottle_neck(ds)
+        ds = F.interpolate(ds, size=(h, w), mode='bilinear')
+        ds = self.output(ds)
+        return ds * x
